4298dbf994796954ec29ad6071fa69a525cff910
Valerie <leraorlova21@gmail.com>
2020-09-21 17:18:47 +0300

    Delete OMTP.R

--- OMTP.R
+++
@@ -1,8 +0,0 @@
-
-
-plot(sub$Ht (m), 
-     sub$Crown diameter (m),
-     col="red", 
-     xlab="Высота дерева, м", 
-     ylab = "Диаметр кроны, м", 
-     main = "Зависимость диаметра от высоты")
